da7bb6635685dfea758b58bf1f9e8b0a3c410e64
Sofia Cerminati <sofiacermi@hotmail.com>
2024-09-06 22:54:17 -0500

    First

---
+++ src/public/js/index.js
@@ -0,0 +1,145 @@
+// Conectar al servidor de Socket.io
+const socket = io();
+
+// Escuchar eventos de conexión y desconexión
+socket.on("connect", () => {
+  console.log("Cliente conectado al servidor");
+});
+
+socket.on("disconnect", () => {
+  console.log("Cliente desconectado del servidor");
+});
+
+// Función para crear un nuevo carrito
+const createNewCart = async () => {
+  try {
+    const response = await fetch("/api/carts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      localStorage.setItem("cartId", data.newCart._id);
+      socket.emit("cartId", data.newCart._id);
+      tostada("!Bienvenido! Tu carrito te espera");
+      console.log(`Nuevo carrito creado con ID: ${data.newCart._id}`);
+      return data.newCart._id;
+    } else {
+      console.error("Error al crear el carrito:", response.statusText);
+      return null;
+    }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    return null;
+  }
+};
+
+// Verificar si el ID del carrito ya está en localStorage
+const getCartId = async () => {
+  let cartId = localStorage.getItem("cartId");
+
+  if (!cartId) {
+    cartId = await createNewCart();
+  }
+  socket.emit("cartId", cartId);
+
+  return cartId;
+};
+
+//Función de cantidad total en el carrito para obtener el cart count.
+const getQT = async () => {
+  let cartId = localStorage.getItem("cartId");
+  try {
+    const response = await fetch(`/api/carts/${cartId}/QT`);
+    if (response.ok) {
+      const data = await response.json();
+      cartCount.innerText = data.totalProductos;
+    } else {
+      console.error(`Error al cargar QT: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+const cartCount = document.getElementById("cartCount");
+if (cartCount) {
+  getQT();
+}
+
+// Escuchar actualizaciones de productos desde el servidor
+socket.on("Cart Update", (updatedCart) => {
+  console.log("Carrito actualizado:", updatedCart);
+  const cartCount = document.getElementById("cartCount");
+  if (cartCount) {
+    getQT();
+  }
+});
+
+// Función para actualizar el enlace del carrito
+const updateCartLink = async () => {
+  const cartId = await getCartId();
+  if (cartId) {
+    const cartLink = document.getElementById("cartLink");
+    cartLink.href = `/carts/${cartId}`;
+    console.log(`Carrito ya existente con ID: ${cartId}`);
+  }
+};
+
+// Función para agregar un producto al carrito
+const addToCart = async (productId) => {
+  const cartId = await getCartId();
+  if (cartId) {
+    try {
+      const response = await fetch(
+        `/api/carts/${cartId}/product/${productId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ productId }),
+        }
+      );
+
+      if (response.ok) {
+        tostada("Producto agreado al carrito.");
+        socket.emit("Product Update", productId);
+      } else {
+        throw new Error("No se pudo agregar el producto al carrito");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      tostada("Error al agregar el producto al carrito");
+    }
+  }
+};
+
+// Escuchar el evento Product Update y actualizar la vista
+socket.on("Product Update", (updatedProduct) => {
+  console.log("Producto Actualizado:", updatedProduct);
+
+  const stockElement = document.querySelector(
+    `p[data-product-ids="${updatedProduct._id}"]`
+  );
+
+  if (stockElement) {
+    // Actualizar el stock en la vista
+    stockElement.innerHTML = `Stock: ${updatedProduct.stock}`;
+  }
+});
+
+// Añadir eventos a los botones de "Agregar al Carrito"
+document.addEventListener("DOMContentLoaded", () => {
+  updateCartLink();
+
+  document.querySelectorAll(".add-to-cart").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.getAttribute("data-product-id");
+      addToCart(productId);
+    });
+  });
+});
---
+++ src/router/views.router.js
@@ -0,0 +1,139 @@
+import express from "express";
+
+import cartsModel from "../models/carts.model.js";
+import productsModel from "../models/products.model.js"; 
+
+const router = express.Router();
+
+// Ruta para renderizar la vista index.handlebars con paginación
+router.get("/", async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 10 ? 10 : limit;
+    const sort = req.query.sort; // Obtén el parámetro de ordenación
+    const category = req.query.category; // Obtén el parámetro de categoría
+
+    let filter = {};
+    if (category) {
+      filter.category = category;
+    }
+
+    let sortOrder;
+    let result;
+    if (sort) {
+      sortOrder = sort === "desc" ? -1 : 1;
+      result = await productsModel.paginate(filter, {
+        page,
+        limit,
+        sort: { price: sortOrder }, // Ordenar por precio
+      });
+    } else {
+      result = await productsModel.paginate(filter, {
+        page,
+        limit,
+      });
+    }
+
+    // Obtener todas las categorías para el filtro
+    const allCategories = await productsModel.distinct("category");
+
+    result.sort = sort;
+    result.category = category;
+    result.categories = allCategories;
+    result.prevLink = result.hasPrevPage
+      ? `/?page=${result.prevPage}${limit < 10 ? `&limit=${limit}` : ""}${
+          sort ? `&sort=${sort}` : ""
+        }${category ? `&category=${category}` : ""}`
+      : "";
+    result.nextLink = result.hasNextPage
+      ? `/?page=${result.nextPage}${limit < 10 ? `&limit=${limit}` : ""}${
+          sort ? `&sort=${sort}` : ""
+        }${category ? `&category=${category}` : ""}`
+      : "";
+    result.isValid = !(page <= 0 || page > result.totalPages);
+
+    res.render("index", result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error al cargar los productos." });
+  }
+});
+
+// Ruta para mostrar los detalles de un producto
+router.get("/products/:pid", async (req, res) => {
+  try {
+    const idProduct = req.params.pid;
+
+    const product = await productsModel.findOne({ _id: idProduct });
+
+    if (product) {
+      res.render("productDetail", {
+        product,
+      });
+    } else {
+      res.status(404).json({ msg: "Producto no encontrado." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error al obtener el producto." });
+  }
+});
+
+//Manejo de views de realtimeproducts
+router.get("/realtimeproducts", async (req, res) => {
+  try {
+    const products = await productsModel.find({}); // Cargar todos los productos desde la base de datos
+    res.render("realtimeproducts", {
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error al cargar los productos." });
+  }
+});
+
+// Función para llenar el carrito con productos
+const populateCarrito = async (carrito) => {
+  return carrito.populate({
+    path: "products.product",
+    select: "id title price stock",
+  });
+};
+
+// Ruta para mostrar el contenido del carrito
+router.get("/carts/:cid", async (req, res) => {
+  try {
+    const cartId = req.params.cid;
+    let carritoEncontrado = await cartsModel.findOne({ _id: cartId });
+
+    if (!carritoEncontrado) {
+      return res.status(404).render("error", { msg: "Carrito no encontrado." });
+    }
+
+    carritoEncontrado = await populateCarrito(carritoEncontrado);
+    carritoEncontrado = {
+      ...carritoEncontrado.toObject(),
+      products: carritoEncontrado.products.map((product) => ({
+        ...product.product.toObject(),
+        quantity: product.quantity,
+      })),
+    };
+
+    let totalPrice = carritoEncontrado.products.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+
+    totalPrice = totalPrice.toFixed(2);
+
+    res.render("cart", {
+      cart: carritoEncontrado,
+      totalPrice,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Error al obtener el carrito." });
+  }
+});
+
+export default router;
